4b568e23a9f1f9f98389daca6d4f5032e3b39e47
devanshtyagi26 <tyagidevansh3@gmail.com>
2025-02-16 18:53:33 +0530

    Clonned trials

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,35 @@
-import { useRef } from "react";
-import * as ts from "@tensorflow/tfjs";
-import * as handpose from "@tensorflow-models/handpose";
+import { useRef, useEffect } from "react";
 import Webcam from "react-webcam";
-import { Drawing } from "./Drawing";
 
 function App() {
   const webRef = useRef(null);
   const canvasRef = useRef(null);
 
-  const runHandPose = async () => {
-    const network = await handpose.load();
-    console.log("Handpose loaded");
-
-    setInterval(() => {
-      detectHand(network);
-    }, 100);
-  };
-  const detectHand = async (network) => {
+  const detectHand = () => {
     if (
-      typeof webRef.current !== "undefined" &&
-      webRef.current !== null &&
-      webRef.current.video.readyState === 4
+      webRef.current &&
+      webRef.current.video.readyState === 4 // Ensure video is fully loaded
     ) {
+      console.log("Webcam video is ready");
+
       const video = webRef.current.video;
-      const videoWidth = webRef.current.video.videoWidth;
-      const videoHeight = webRef.current.video.videoHeight;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
 
-      webRef.current.video.width = videoWidth;
-      webRef.current.video.height = videoHeight;
-
+      // Set canvas size to match video
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      const hand = await network.estimateHands(video);
-      console.log(hand);
       const ctx = canvasRef.current.getContext("2d");
-      Drawing(hand, ctx);
+      console.log("Canvas context:", ctx);
     }
   };
-  runHandPose();
+
+  useEffect(() => {
+    const interval = setInterval(detectHand, 1000); // Call detectHand every second
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, []);
+
   return (
     <>
       <Webcam
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  assetsInclude: ["**/*.task"], // Tell Vite to treat .task as an asset
+});
