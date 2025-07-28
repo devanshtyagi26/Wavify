e955922faf4440c158c40503a7688194c0ce67f8
devanshtyagi26 <tyagidevansh3@gmail.com>
2025-07-28 16:17:30 +0530

    Restarting with next

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-
-  assetsInclude: ["**/*.task"], // Tell Vite to treat .task as an asset
-});
